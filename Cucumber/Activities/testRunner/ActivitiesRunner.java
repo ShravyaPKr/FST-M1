677fc4cfcbc25eaddca1e9a5034f3cee29566fce
ShravyaPKr <129042228+ShravyaPKr@users.noreply.github.com>
2024-06-29 20:14:32 +0530

    Update ActivitiesRunner.java

--- Cucumber/Activities/testRunner/ActivitiesRunner.java
+++ Cucumber/Activities/testRunner/ActivitiesRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
     features = "src/test/java/Features",
     glue = {"stepDefinition"},
     // Below tags will run Activity1 to Activity 5 at once
-    tags = "@activity1 or @activity2 or @activity3 or @activity4 or @activity5 or @activity6",
+    tags = "@activity1 or @activity2 or @activity3 or @activity4 or @activity5",
     //tags = "@SmokeTest", // Run separately for Activity 6
     plugin = {"pretty"},
     //plugin = {"html: test-reports"}, // Run separately for Activity 6
